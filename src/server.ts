7133240e3ce37ce55aaf4993757ea806ab22e480
Layla Rodas <rodas.layla@gmail.com>
2024-04-19 12:54:30 +0200

    install sequelize, connect database

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,23 @@
 import express from "express";
 import router from "./router";
+import db from './config/db';
+
+
+
+//connect to db
+async function connectDB() {
+    try {
+        await db.authenticate();
+        db.sync();
+        console.log('Connected successfully to db')
+
+    } catch (error) {
+        console.log(error)
+        console.log('Failed to connect to db')
+    }
+}
+
+connectDB();
 
 
 const server = express();
