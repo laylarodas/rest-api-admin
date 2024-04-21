675c6e7b6c4c1c153ccb052ccb595e338e2c6abc
Layla Rodas <rodas.layla@gmail.com>
2024-04-21 13:21:19 +0200

    validations product test

--- src/__tests__/product.test.ts
+++ src/__tests__/product.test.ts
@@ -3,6 +3,20 @@ import server from "../server";
 
  
 describe('POST /api/products', () => {
+
+    it('should display validation error', async () => {
+        const response = await request(server)
+            .post('/api/products')
+            .send({});//simulation send empty data
+
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('errors');
+        expect(response.body.errors).toHaveLength(5);//4 errors
+
+        expect(response.status).not.toBe(404);
+        expect(response.body).not.toHaveLength(4);
+    });
+
     it('should create a new product', async () => {
        const response = await request(server)
             .post('/api/products')
@@ -17,6 +31,6 @@ describe('POST /api/products', () => {
 
         expect(response.status).not.toBe(404);
         expect(response.status).not.toBe(200);
-        expect(response.body.data).not.toHaveProperty('error');
+        expect(response.body.data).not.toHaveProperty('errors');
     });
 });
