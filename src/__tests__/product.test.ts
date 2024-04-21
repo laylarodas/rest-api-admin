bf8682d150cec7eb7fa33f60b04a47322d71d6a0
Layla Rodas <rodas.layla@gmail.com>
2024-04-21 13:47:25 +0200

    clear database

--- src/__tests__/product.test.ts
+++ src/__tests__/product.test.ts
@@ -11,10 +11,42 @@ describe('POST /api/products', () => {
 
         expect(response.status).toBe(400);
         expect(response.body).toHaveProperty('errors');
-        expect(response.body.errors).toHaveLength(5);//4 errors
+        expect(response.body.errors).toHaveLength(5);//5 errors
 
         expect(response.status).not.toBe(404);
-        expect(response.body).not.toHaveLength(4);
+        expect(response.body.errors).not.toHaveLength(4);
+    });
+
+    it('should validate that the price is greater than 0', async () => {
+        const response = await request(server)
+            .post('/api/products')
+            .send({
+                name: 'product-test',
+                price: 0
+            });
+
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('errors');
+        expect(response.body.errors).toHaveLength(1);
+
+        expect(response.status).not.toBe(404);
+        expect(response.body.errors).not.toHaveLength(2);
+    });
+
+    it('should validate that the price is a number and greater than 0', async () => {
+        const response = await request(server)
+            .post('/api/products')
+            .send({
+                name: 'product-test',
+                price: 'price-test'
+            });
+
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('errors');
+        expect(response.body.errors).toHaveLength(2);
+
+        expect(response.status).not.toBe(404);
+        expect(response.body.errors).not.toHaveLength(3);
     });
 
     it('should create a new product', async () => {
