7403dd37069efbf162ea4efff5fb9d45ce9d1ce8
Layla Rodas <rodas.layla@gmail.com>
2024-04-22 23:10:08 +0200

    updated product test

--- src/__tests__/product.test.ts
+++ src/__tests__/product.test.ts
@@ -125,3 +125,91 @@ describe('GET /api/products/:id', () => {
         });
     }
 );
+
+describe('PUT /api/products/:id', () => {
+    it('should display validation error messages when updating a product', async () => {
+        const response = await request(server)
+            .put('/api/products/1')
+            .send({});//simulation send empty data
+
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('errors');
+        expect(response.body.errors).toBeTruthy();
+        expect(response.body.errors).toHaveLength(6);
+
+        expect(response.status).not.toBe(200);
+        expect(response.body).not.toHaveProperty('data');
+    })
+
+
+    it('should validate that the price is greater than 0', async () => {
+        const response = await request(server)
+            .put('/api/products/1')
+            .send({
+                name: 'product-test',
+                availability: true,
+                price: 0
+            });
+
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('errors');
+        expect(response.body.errors).toBeTruthy();
+        expect(response.body.errors).toHaveLength(1);
+        expect(response.body.errors[0].msg).toBe('Price has to be greater than 0');
+
+        expect(response.status).not.toBe(200);
+        expect(response.body).not.toHaveProperty('data');
+    });
+
+    it('should check a valid ID in the URL', async () => {
+        const response = await request(server)
+            .put('/api/products/not-valid-url')
+            .send({
+                name: 'product-test',
+                availability: true,
+                price: 100
+            });
+
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('errors');
+        expect(response.body.errors).toHaveLength(1);
+        expect(response.body.errors[0].msg).toBe('Id has to be integer');
+        
+    });
+
+    it('should return a 404 response for a non-existent product', async () => {
+        const productId = 2000;
+        const response = await request(server)
+            .put(`/api/products/${productId}`)
+            .send({
+                name: 'product-test',
+                availability: true,
+                price: 100
+            });
+
+        expect(response.status).toBe(404);
+        expect(response.body).toHaveProperty('error');
+        expect(response.body.error).toBe('Product not found');
+
+        expect(response.status).not.toBe(200);
+        expect(response.body).not.toHaveProperty('data');
+       
+    });
+
+
+    it('should update an existing product with valid data', async () => {
+        const response = await request(server)
+            .put('/api/products/1')
+            .send({
+                name: 'product-test',
+                availability: true,
+                price: 100
+            });
+
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('data');
+
+        expect(response.status).not.toBe(404);
+        expect(response.body).not.toHaveProperty('errors');
+    });
+});
