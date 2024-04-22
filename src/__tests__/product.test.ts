3edaae6a08af86809c8bc1857fc0d2aa618cfa9d
Layla Rodas <rodas.layla@gmail.com>
2024-04-22 21:18:27 +0200

    a single product test

--- src/__tests__/product.test.ts
+++ src/__tests__/product.test.ts
@@ -90,3 +90,38 @@ describe('GET /api/products', () => {
     });
 }
 );
+
+describe('GET /api/products/:id', () => {
+    
+        it('should return a 404 response for a non-existent product', async () => {
+            const productId = 2000;
+            const response = await request(server)
+                .get(`/api/products/${productId}`);
+
+            expect(response.status).toBe(404);
+            expect(response.body).toHaveProperty('error');
+            expect(response.body.error).toBe('Product not found');
+        });
+    
+        it('should check a valid ID in the URL', async () => {
+            const response = await request(server)
+                .get('/api/products/not-valid-url');
+    
+            expect(response.status).toBe(400);
+            expect(response.body).toHaveProperty('errors');
+            expect(response.body.errors).toHaveLength(1);
+            expect(response.body.errors[0].msg).toBe('Id has to be integer');
+            
+        });
+    
+        it('GET a JSON response for a single product', async () => {
+            const response = await request(server)
+                .get('/api/products/1');
+    
+            expect(response.status).toBe(200);
+            expect(response.body).toHaveProperty('data');
+    
+            expect(response.body).not.toHaveProperty('errors');
+        });
+    }
+);
