f03412398b4efbf3e19e1535fceb0fdda74af55a
Layla Rodas <rodas.layla@gmail.com>
2024-04-29 11:45:54 +0200

    allow permitions with cors

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import router from "./router";
 import db from './config/db';
 import colors from 'colors';
+import cors, { CorsOptions } from 'cors';
 import swaggerUi from 'swagger-ui-express';
 import swaggerSpec, { swaggerUiOptions } from "./config/swagger";
 
@@ -23,6 +24,17 @@ connectDB();
 //express instance
 const server = express();
 
+//allow connections
+const corsOptions : CorsOptions = {
+    origin: function(origin, callback){
+        if(origin == process.env.FRONTEND_URL){
+            callback(null, true)
+        }else{
+            callback(new Error('CORS error'))
+        }
+    }
+}
+server.use(cors(corsOptions))
 
 //read data from form
 server.use(express.json());
